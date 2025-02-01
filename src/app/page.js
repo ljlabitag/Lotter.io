93c4d6a050151187be0eb6cadf1f087e97fe7f8c
ljlabitag <ljlabitagdev@gmail.com>
2025-02-01 21:22:09 +0800

    FOL-5 Initial commit for Lotter.io landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <p>Home</p>
+    <main>
+    {/* Welcome Section */}
+      <div className="flex flex-row">
+        <div className="basis-3/5 pl-10 pt-6">
+          <h2 className="text-6xl font-bold mb-4">
+            Your Ultimate <br/>Companion in Playing <br/>the Philippine Lottery!
+          </h2>  
+          <p className="text-2xl">
+            Welcome to Lotter.io! We provide you with the latest lotto results,
+            <br/>lucky picks, and more! Stay updated, play smarter, and 
+            <br/>never miss a chance to win big!
+          </p>
+        </div> 
+        <div className="flex basis-2/5 justify-center items-center pr-14">
+          <Image 
+            src="/lottoballs.jpg" 
+            alt="lotter.io" 
+            width={288} 
+            height={288} 
+            className="w-[350px] h-[350px] rounded-full object-cover object-center  " 
+          />
+        </div>
       </div>
 
+    {/* Featured Games Section */}
       <div>
         <p>Games</p>
       </div>
@@ -17,6 +38,6 @@ export default function Home() {
       <div>
         <p>Contact Us</p>
       </div>
-    </div>
+    </main>
   );
 }
